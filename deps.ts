b67d785881a0d967adf92950fd8313af2d49cc1b
Santiago <santibelca@gmail.com>
2021-01-20 00:32:58 -0500

    fixed dependency

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { opine, json } from "https://deno.land/x/opine@1.1.0/mod.ts";
 export { opineCors } from "https://deno.land/x/cors/mod.ts";
-export { v4 } from "https://deno.land/std@$0.84.0/uuid/mod.ts";
+export { v4 } from "https://deno.land/std/uuid/mod.ts";
 
